92bb144fce63bda98c7e1ccf36096b2f90311626
supersiq <jonathan.broms@gmail.com>
2022-12-06 22:54:54 +0100

    Now everything is broken in aoc1. :/)

--- a_o_c_1.c
+++ a_o_c_1.c
@@ -2,30 +2,22 @@
 # include <stdlib.h>
 
 int main(void){
-
-    printf("test\n");
-
     FILE *in_file;
     int number1;
 
-    in_file = fopen("input_a_o_c_1.txt", "r");
-    for(int i=0; i!=14; i++){
-        if (in_file == NULL)
-        {
-            printf("Can't open file for reading.\n");
-            return 0;
-        }
-        else
-        {
-            fscanf(in_file, "%d", &number1);
-            if (in_file == '\n'){
-                i = i - 1;
-                printf("bonjour");
-            }
-            printf("Iteration: %d, %d \n",i , number1);
-            
-        }
+    in_file = fopen("number.txt", "r");
+    if (in_file == NULL)
+    {
+        printf("Can't open file for reading.\n");
+        return 0;
     }
+
+    fscanf(in_file, "%d", &number1);
+    if (in_file == '%\n'){
+        printf("bonjour");
+        return 0;
+    }
+    printf("i made it here");
     fclose(in_file);
     return 0;
 }
