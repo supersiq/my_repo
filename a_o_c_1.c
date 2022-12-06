783e6c1bffcef59f01a6e4867d8d3aac6f980aa0
supersiq <jonathan.broms@gmail.com>
2022-12-06 01:33:18 +0100

    adding aoc files

--- a_o_c_1.c
+++ a_o_c_1.c
@@ -1,13 +1,25 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-# include "input_a_o_c_1.txt"
-
 int main(void){
 
-    printf("test")
+    printf("test");
 
-    
+    FILE *in_file;
+    int number1;
 
+    in_file = fopen("number.txt", "r");
+    for(int i=0; i!=3; i++){
+        if (in_file == NULL)
+        {
+            printf("Can't open file for reading.\n");
+        }
+        else
+        {
+            fscanf(in_file, "%d", &number1);
+            printf("%d \n", number1);
+        }
+    }
+    fclose(in_file);
     return 0;
 }
