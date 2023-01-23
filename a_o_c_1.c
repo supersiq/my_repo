d96aed1745acf9182d3f4fb3c75e38047c432d03
supersiq <jonathan.broms@gmail.com>
2023-01-23 23:58:19 +0100

    zzzz

--- a_o_c_1.c
+++ a_o_c_1.c
@@ -3,7 +3,8 @@
 
 int main(void){
     FILE *in_file;
-    int number1;
+    //Array for storing the numbers.
+    int numbers[1024];
 
     in_file = fopen("number.txt", "r");
     if (in_file == NULL)
@@ -12,12 +13,24 @@ int main(void){
         return 0;
     }
 
-    fscanf(in_file, "%d", &number1);
-    if (in_file == '%\n'){
-        printf("bonjour");
-        return 0;
-    }
-    printf("i made it here  hello   ");
+
+    int i = 0;
+    fscanf(in_file, "%d", &numbers[i]);
+    printf("%d \n", numbers[i]);
+    // Want to iterate in between the lines in numbers.txt
+    while(fscanf(in_file, "%d", &numbers[i]) != NULL){
+    printf("%d \n", numbers[i]);
+        i = i +1;
+            if(i==4){
+                //Exit
+                fclose(in_file);
+                printf("damn");
+                return 0;
+            }
+
+        
+    }       
+    printf("i made it here  hello");
     fclose(in_file);
     return 0;
 }
