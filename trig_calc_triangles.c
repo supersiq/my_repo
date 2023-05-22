c8b62fcac20e55bc1b3a3ce25466011feb59dfb3
supersiq <jonathan.broms@gmail.com>
2023-05-22 21:26:16 +0200

    small calculator for trig_oriented structure

---
+++ trig_calc_triangles.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+
+double profile_hypo;
+double profile_hypo;
+double profile_hypo;
+double profile_hypo;
+
+
+    while(profile_height >= 80){
+
+    }
+
+
+    return 0;
+}
