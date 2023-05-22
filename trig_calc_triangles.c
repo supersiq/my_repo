52e04b6ae0e5cc27ea61085a91ed380c0f79b5e8
supersiq <jonathan.broms@gmail.com>
2023-05-22 21:58:42 +0200

    lilmsg

--- trig_calc_triangles.c
+++ trig_calc_triangles.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 double hypo(double base, double height){
 
     double squared = (pow(base, 2) + pow(height, 2));
@@ -14,17 +13,28 @@ double hypo(double base, double height){
 
 int main (void){
 
-    double profile_base = 1;
-    double profile_height = 1;
+
+    double initial_base = 120 - ((27.5*2)/3.14159265358979323846);
+    printf("%.1f", initial_base);
+    double profile_base;
+    double profile_height = 180;
 
     double profile_hypo = hypo(profile_base, profile_height);
-    printf("this should be sqrt2 %.1f \n", profile_hypo);
+//    printf("this should be sqrt2 %.2f \n", profile_hypo);
 
 
 
-    double plane_hypo;
     double plane_base;
     double plane_height;
+    double plane_hypo = hypo(plane_base, plane_height);
+
+
+    int i = 0;
+    while(profile_height != 80){
+        i = i + 1;
+        printf("iteration: %d \n", i);
+        profile_height -= 20;    
+    }
 
 
     return 0;
