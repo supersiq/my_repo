48b11564d6abc7aae43ffdbe8e6b674eb75ee646
supersiq <jonathan.broms@gmail.com>
2023-05-22 21:36:20 +0200

    pythagorean func and start

--- trig_calc_triangles.c
+++ trig_calc_triangles.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+
+double hypo(double base, double height){
+
+    double squared = (pow(base, 2) + pow(height, 2));
+    double hypothenuse = sqrt(squared);
+
+    return hypothenuse;
+}
+
 
 int main (void){
 
-double profile_hypo;
-double profile_hypo;
-double profile_hypo;
-double profile_hypo;
+    double profile_base = 1;
+    double profile_height = 1;
+
+    double profile_hypo = hypo(profile_base, profile_height);
+    printf("this should be sqrt2 %.1f \n", profile_hypo);
 
 
-    while(profile_height >= 80){
 
-    }
+    double plane_hypo;
+    double plane_base;
+    double plane_height;
 
 
     return 0;
