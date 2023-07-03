95d213d03f30e676b1cbc540894e4031da52cac3
supersiq <jonathan.broms@gmail.com>
2023-07-03 15:45:07 +0200

    default

---
+++ dota_2_hack.js
@@ -0,0 +1,11 @@
+// 1. Get data
+
+// 1.1 Get pages to parse for all heroes
+// https://www.dotabuff.com/heroes
+
+
+
+// 1.2 For each hero, fetch common positions and counter stats
+
+
+    
---
+++ hell_js.js
@@ -0,0 +1,11 @@
+// 1. Get data
+
+// 1.1 Get pages to parse for all heroes
+
+// 1.2 Allr heroes counter stats
+// 
+// 
+
+// 1.2 Counter stats for all pos 1 heroes
+
+    
